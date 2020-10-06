062d2eb3fb39dbca171ca96c7963067f46a4e225
raheltsehaye <raheltsehaye@localhost>
2020-10-05 18:06:18 -0700

    first changes

---
+++ Assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+/* this is a class */
+public class AddingMachine {
+  private int total;
+   // this is a constructor method */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  /*  returns the total value */
+  public int getTotal () {
+    return 0;
+  }
+  /*  adds the new value to total */
+  public void add (int value) {
+  }
+ /*  subtracts the new value total */
+  public void subtract (int value) {
+  }
+  /* returns/prints out the results */
+  public String toString () {
+    return "";
+  }
+  /* clears the memory and resets the total to zero */
+  public void clear() {
+  }
+}
